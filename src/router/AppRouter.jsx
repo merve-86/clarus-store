3b4e3133cd1e10fd2a50dabfc66a1d8b5f396080
merve <merveaydin86@hotmail.com>
2024-04-24 09:36:39 +0300

    first commit

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "../pages/Login";
+import NotFound from "../pages/NotFound";
+import Products from "../pages/Products";
+import About from "../pages/About";
+import Home from "../pages/Home";
+import Navbar from "../components/Navbar";
+import PrivateRouter from "./PrivateRouter";
+
+const AppRouter = () => {
+  return (
+    // <BrowserRouter>
+    <>
+      {/* <Navbar /> */}
+      <Routes>
+        <Route path="/" element={<Login />} />
+
+        <Route path="/dashboard" element={<PrivateRouter />}>
+          <Route path="" element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="/dashboard/products" element={<Products />} />
+        </Route>
+
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
+    // </BrowserRouter>
+  );
+};
+
+export default AppRouter;
---
+++ src/router/PrivateRouter.jsx
@@ -0,0 +1,19 @@
+import React, { useContext } from "react";
+import { AuthContext } from "../context/AuthProvider";
+import { Outlet } from "react-router-dom";
+import { Navigate } from "react-router-dom";
+import Navbar from "../components/Navbar";
+
+const PrivateRouter = () => {
+  const { user } = useContext(AuthContext);
+  return user ? (
+    <>
+      <Navbar />
+      <Outlet />
+    </>
+  ) : (
+    <Navigate to="/" />
+  );
+};
+
+export default PrivateRouter;
